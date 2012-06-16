697ab5d63efa859fd6dc631f0aa808140cb5d778
rudie2004 <rudie2004@home.nl>
2012-06-16 03:04:50 +0200

    9

---
+++ config.php
@@ -0,0 +1,25 @@
+<?php
+// enter navigation bar links
+$navlink;
+$navlink["Home"] = "/Zumron/main.php";
+//$navlink["XBMC"] = "http://xmbc.local.lan:8080";
+//$navlink["Sickbeard"] = "http://sickbeard.local.lan:8081";
+//$navlink["Couch Potato"] = "http://couchpotato.local.lan:8082";
+//$navlink["Sabnzbd"] = "http://sabnzbd.local.lan:8085";
+
+
+// enter subnavigation bar links
+$subnavlink["IMDB"] = "http://www.imdb.com";
+$subnavlink["MovieMeter"] = "http://www.MovieMeter.nl";
+$subnavlink["Newzbin"] = "http://www.newzbin2.es";
+$subnavlink["IMDB"] = "http://www.imdb.com";
+$subnavlink["Nzbindex"] = "http://www.nzbindex.nl";
+
+// enter other links
+$settingslink["Settings"] = "/Zumron/Settings";
+
+$settings["sabnzbdip"] = "LOCALHOST";
+$settings["sabnzbdport"] = "8080";
+$settings["sabnzbdapi"] = "CHANGEAPIKEY";
+
+?>
--- main.php
+++ main.php
@@ -1,22 +1,58 @@
 <?php
+include_once "components/sabnzbd/api.php";	
+include "config.php"; 
 $url_folder = substr(substr($_SERVER["REQUEST_URI"],1), 0, strpos(substr($_SERVER["REQUEST_URI"],1), "/"));
+$sabnzbdip = $settings["sabnzbdip"];
+$sabnzbdport = $settings["sabnzbdport"];
+$sabnzbdapi = $settings["sabnzbdapi"];
 ?>
 <link rel="stylesheet" type="text/css" href="/<?php echo $url_folder ;?>/templates/zumron_base/styles.css" />
-<body>
+<body><center>
 	<div class="all">
 		<div class="column-middle" id="column1">
 			<div class="box1">
-				<div class="dragbox" id="item1" >
-					<center><h2></h2></center>
-					<div class="dragbox-content" ></div>
+				<div class="dragbox">
+					<center><h1>Sabnzbd current queue</h1></center>
+						<div class="dragbox-content">
+							<?php
+								$sabnzbd = new sabnzbd;
+								$xml1 = simplexml_load_file('http://$sabnzbdip:$sabnzbdport/api?mode=queue&start=START&limit=LIMIT&output=xml&apikey=$sabnzbdportapi');  
+								$movie = $sabnzbd->getdownloadqueueinfo($xml1);
+								Print_r($movie);
+							?>		
+						</div>
 				</div>
 			</div>
-			<div class="box2">
-				<div class="dragbox" id="item2" >
-					<center><h2></h2></center>
+			<div class="box1">
+				<div class="dragbox">
+					<center><h1>last 5 downloads</h1></center>
+						<div class="dragbox-content">
+							<?php
+								$sabnzbd = new sabnzbd;
+							
+								$xml = simplexml_load_file("http://$sabnzbdip:$sabnzbdport/api?mode=history&start=START&limit=5&output=xml&apikey=$sabnzbdapi"); 
+								$xml1 = simplexml_load_file('http://$sabnzbdip:$sabnzbdport/api?mode=queue&start=START&limit=LIMIT&output=xml&apikey=$sabnzbdportapi'); 
+								$movie = $sabnzbd->getdownloadmovieinfo($xml,$xml1);
+								Print_r($movie);
+							?>
+						</div>
+				</div>
+			</div>
+			<div class="box1">
+				<div class="dragbox">
+					<center><h1>Sabnzbd Statistics</h1></center>
+						<div class="dragbox-content">
+							<?php
+								$sabnzbd = new sabnzbd;
+								$xml = simplexml_load_file("http://$sabnzbdip:$sabnzbdport/api?mode=history&start=START&limit=5&output=xml&apikey=$sabnzbdapi"); 
+								$xml1 = simplexml_load_file('http://$sabnzbdip:$sabnzbdport/api?mode=queue&start=START&limit=LIMIT&output=xml&apikey=$sabnzbdportapi'); 
+								$movie = $sabnzbd->getSizeSpeed($xml,$xml1);
+								Print_r($movie);
+							?>
+						</div>
 				</div>
 			</div>
 		</div>		
-	</div>
+	</div></center>
 </body>
 

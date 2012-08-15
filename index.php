be3a77927059a91c83a40fe18ff40aac95467f70
rudie2004 <rudie2004@home.nl>
2012-08-15 23:29:33 +0200

    0.0.7

--- index.php
+++ index.php
@@ -8,17 +8,28 @@
   		<link rel="stylesheet" type="text/css" href="templates/zumron_base/navcss.php">
   </head>
   <body>
-  		<div id="header">
-  		<div class="clearfix"><a href="http://www.zumron.net"style="border: 0px" target="_blank"><img src="templates/zumron_base/images/zumronlogo.png" /></a></div>
+  		  		<div id="header">
+  			<div class="clearfix"><a href="http://www.zumron.net"style="border: 0px" target="_blank"><img src="templates/zumron_base/images/zumronlogo.png" /></a></div>
 						
 								<div id="menu-content">
 										
 									<ul id="top-menu" class="nav">
 															
-											<?php 	
-												foreach( $navlink as $navlinklabel => $navlinkpath) {
-												echo"          <li><a href='".$navlinkpath."' target=main>".$navlinklabel."</a></li>";
-								   				 }
+											<?php 
+											echo"<li><a href='main.php' target=main>Home</a></li>";
+											
+											mysql_connect($mysql["Server"],$mysql["Username"],$mysql["Password"]);
+											@mysql_select_db($mysql["Database"]) or die( "Unable to select database");
+											$result = mysql_query("select visible_name, value2 FROM Settings where type = 'menu' and value = 'on' ");	
+											
+											
+											while($row = mysql_fetch_array( $result )) {
+												
+												echo"<li><a href='".$row[1]."' target=main>".$row[0]."</a></li>";
+											}
+											
+											
+									echo"<li><a href='settings/index.php' target=main>Settings</a></li>";
 								 			?>
 									</ul>																		
 						
@@ -27,12 +38,20 @@
 							
 							<div id="menu-right-bottom">
 								<ul id="sub-menu" class="nav">
-									<?php
-								
-										foreach( $subnavlink as $subnavlinklabel => $subnavlinkpath) {
-										echo "         <li><a href='".$subnavlinkpath."' target=main>".$subnavlinklabel."</a></li>";
-					    				}
-									?>
+									<?php 					
+											mysql_connect($mysql["Server"],$mysql["Username"],$mysql["Password"]);
+											@mysql_select_db($mysql["Database"]) or die( "Unable to select database");
+											$result = mysql_query("select visible_name, value2 FROM Settings where type = 'submenu' and value = 'on' ");	
+											
+											
+											while($row = mysql_fetch_array( $result )) {
+												
+												echo"<li><a href='".$row[1]."' target=main>".$row[0]."</a></li>";
+											}
+											
+											
+									
+								 			?>
 								</ul>
 							</div>
   		</div>

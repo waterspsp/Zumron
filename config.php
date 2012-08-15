087b2bb77491fca1dcac1cf0bb8b1f7cb2d32e17
root <root@Dlsrv02.(none)>
2012-08-15 16:54:10 +0200

    0.0.5

--- config.php
+++ config.php
@@ -5,17 +5,21 @@ $navlink["Home"] = "main.php";
 //$navlink["XBMC"] = "http://xmbc.local.lan:8080";
 //$navlink["Sickbeard"] = "http://sickbeard.local.lan:8081";
 //$navlink["Couch Potato"] = "http://couchpotato.local.lan:8082";
+//$navlink["Sabnzbd"] = "http://sabnzbd.local.lan:8085";
+//$navlink["Spotweb"] = "http://spotweb.local.lan/spotweb";
 //$navlink["Headphones"] = "http://headphones.local.lan:8085";
 //$navlink["Lazy librarian"] = "http://lazylibrarian.local.lan:8085";
-//$navlink["Sabnzbd"] = "http://sabnzbd.local.lan:8085";
 
 
 // enter subnavigation bar links
 $subnavlink["IMDB"] = "http://www.imdb.com";
 $subnavlink["MovieMeter"] = "http://www.MovieMeter.nl";
 $subnavlink["Newzbin"] = "http://www.newzbin2.es";
+$subnavlink["Nzbmatrix"] = "http://www.nzbmatrix.com";
 $subnavlink["IMDB"] = "http://www.imdb.com";
 $subnavlink["Nzbindex"] = "http://www.nzbindex.nl";
+$subnavlink["Tvrage"] = "http://www.tvrage.com";
+
 
 //Enter here the application settings
 $settings["sabnzbdip"] = "LOCALHOST";

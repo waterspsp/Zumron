765625fe2026ef0d0852d0be9b4f46fa67300460
rudie2004 <rudie2004@home.nl>
2012-08-16 22:20:52 +0200

    0.0.9

--- install.php
+++ install.php
@@ -5,14 +5,16 @@ $mysqlImportFilename ='zumron.sql';
 
 //DONT EDIT BELOW THIS LINE
 //Export the database and output the status to the page
-$command='mysql -h' .$mysql["server"].' -u' .$mysql["Username"].' -p' .$mysql["Password"] .' ' .$mysql["Database"] .' < ' .$mysqlImportFilename;
+$command='mysql -h '.$mysql["Server"].' -u'.$mysql["Username"].' -p'.$mysql["Password"] .' < ' .$mysqlImportFilename;
+          
 exec($command,$output=array(),$worked);
 switch($worked){
     case 0:
-        echo 'Import file <b>' .$mysqlImportFilename .'</b> successfully imported to database <b>' .$mysqlDatabaseName .'</b>';
+        echo 'Zumron is successfully installed <br> Remove install.php and zumron.sql and click <a href="./"> here to launch zumron</a>'	 ; 
         break;
     case 1:
-        echo 'There was an error during import. Please make sure the import file is saved in the same folder as this script and check your values:<br/><br/><table><tr><td>MySQL Database Name:</td><td><b>' .$mysqlDatabaseName .'</b></td></tr><tr><td>MySQL User Name:</td><td><b>' .$mysqlUserName .'</b></td></tr><tr><td>MySQL Password:</td><td><b>NOTSHOWN</b></td></tr><tr><td>MySQL Host Name:</td><td><b>' .$mysql["server"] .'</b></td></tr><tr><td>MySQL Import Filename:</td><td><b>' .$mysqlImportFilename .'</b></td></tr></table>';
+        echo 'There was an error during import. Please make sure the import file 
+        is saved in the same folder as this script and check your values:<br/><br/><table>
+        <tr><td>MySQL Database Name:</td><td><b>' .$mysql["Database"]  .'</b></td></tr><tr><td>MySQL User Name:</td><td><b>' .$mysql["Username"]  .'</b></td></tr><tr><td>MySQL Password:</td><td><b>NOTSHOWN</b></td></tr><tr><td>MySQL Host Name:</td><td><b>' .$mysql["Server"] .'</b></td></tr><tr><td>MySQL Import Filename:</td><td><b>' .$mysqlImportFilename .'</b></td></tr></table><br><br> It can also be that there is already a database with the same name installed';
         break;
 }
-?>   
